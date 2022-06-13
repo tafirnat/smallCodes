35b6ce38f75107d5a1420edb853438f8ff697c37
tafirnat <45982173+tafirnat@users.noreply.github.com>
2022-06-13 05:21:49 +0200

    sds

---
+++ urlScript_04.js
@@ -0,0 +1,24 @@
+function newSelectedText(){
+    let sTxt, msg, index, newDef,oldDef, sObj, subKey = {},mp3src='https://cdn.pixabay.com/download/audio/2021/12/03/audio_19b9dafe07.mp3?';
+    sTxt = window.getSelection().toString().trim();
+    sObj = JSON.parse(window.localStorage.getItem('@ri5: neuWorte'));
+
+    try {
+        if(sObj[sTxt] == undefined) throw `notFound`
+        for(subKey in sObj[sTxt]) {oldDef = sObj[sTxt][subKey]}
+        newDef = prompt(`⚠️ ${sTxt} kelimesi listede mevcut.`,oldDef );
+        if(newDef==null || oldDef == newDef) return 
+        delete sObj[sTxt][subKey] 
+    } catch (error) {
+         newDef = prompt(`✅ "${sTxt}" kelimesi listeye eklenecek.`,'Kelimeyi tanimla...');
+        if(newDef==null) return 
+        if (!sObj) sObj = {};
+    }
+        subKey=(new Date()).toLocaleString().replaceAll(' ','_');
+        sObj[sTxt]={}
+        sObj[sTxt][subKey]= newDef;
+        window.localStorage.setItem('@ri5: neuWorte', JSON.stringify(sObj));
+        new Audio(mp3src).play();
+    };
+    window.newSelectedText=newSelectedText
+    newSelectedText()
